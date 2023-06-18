b6f1988ef77e7a62cb5c0cf1834a2e0f317ebb4a
Sagi Haklay <sahaklay@gmail.com>
2023-06-18 18:20:16 +0300

    test

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -44,3 +44,17 @@ def kmeans_pp(points, k):
         centroids.append(new_center)
     return centroids
     
+def read_from_file(fname):
+    list_of_points = []
+    f = open(fname, "r")
+    for line in f:
+        current_line = line.split(",")
+        current_line = [float(x) for x in current_line]
+        list_of_points.append(current_line)
+    return list_of_points
+datapoints = read_from_file('tests/input_1.txt')
+centroids = kmeans_pp(datapoints, 3)
+centroids = kmeans.fit(datapoints, centroids, 600, 0.001)
+for cent in centroids:
+    data = [float("%.4f" % i) for i in cent]
+    print(*data, sep=",")
